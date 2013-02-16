2f9929a2e36923266f6ba0d8fc36f75a0c8511ec
rattmuffen <rattmuffen@gmail.com>
2013-02-16 21:53:05 +0100

    Pretty shaky name suggestions functionality for names that are not in the database.

--- Namnsdagar/src/se/sweddit/namnsdagar/contactspicker/ContactsPickerActivity.java
+++ Namnsdagar/src/se/sweddit/namnsdagar/contactspicker/ContactsPickerActivity.java
@@ -3,9 +3,15 @@ package se.sweddit.namnsdagar.contactspicker;
 import java.util.ArrayList;
 
 import se.sweddit.namnsdagar.DBHelper;
+import se.sweddit.namnsdagar.MainActivity;
 import se.sweddit.namnsdagar.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -19,35 +25,96 @@ public class ContactsPickerActivity extends Activity {
 
 	private ListView listView;
 	private ContactArrayAdapter listAdapter ;
-	
+	private ArrayList<String> acceptableNames;
+
+	AlertDialog suggestionDialog;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contactslist);
 
-		
+		//TODO detta bör givetvis inte göras varje gång 
+		acceptableNames = getAllNames();
+
+
+
 		listView = (ListView) findViewById( R.id.contactsListView);
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			
+
 			@Override
 			public void onItemClick( AdapterView<?> parent, View item, int position, long id) {
-				Contact contact = listAdapter.getItem( position );
-				contact.toggle();
-				
-				ContactViewHolder viewHolder = (ContactViewHolder) item.getTag();
-				viewHolder.getCheckBox().setChecked(contact.getChecked());
+				Contact contact = listAdapter.getItem(position);
+
+
+				if (!acceptableNames.contains(Misc.getFirstName(contact.getName()))) {
+					String[] suggestions = Misc.getSimilarNames(contact.getName(), acceptableNames,5);
+
+					showSuggestionDialog(contact,suggestions);
+				} else {
+					contact.toggle();
+
+
+					ContactViewHolder viewHolder = (ContactViewHolder) item.getTag();
+					viewHolder.getCheckBox().setChecked(contact.getChecked());
+				}
 			}
 		});
 
 		ArrayList<Contact> contactsInDB = getSelected();
 		listAdapter = new ContactArrayAdapter(this,contactsInDB);
-		
+
 		listView.setAdapter(listAdapter);      
 	}
-	
+
+	public void showSuggestionDialog(final Contact c, final String[] suggestions) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setItems(suggestions, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+
+				Toast toast = Toast.makeText(getApplicationContext(), "Selected " + suggestions[which], Toast.LENGTH_SHORT);
+				toast.show();
+
+
+				//ingen aning om det här funkar som det skall...
+				c.setName(suggestions[which]);
+			}
+		});
+
+
+		AlertDialog alertDialog = builder.create();
+		alertDialog.setTitle("Kunde inte hitta " + Misc.getFirstName(c.getName()));
+
+		alertDialog.setIcon(R.drawable.icon);
+		alertDialog.show();
+	}
+
+	private ArrayList<String> getAllNames() {
+		ArrayList<String> namelist = new ArrayList<String>();
+
+		DBHelper dbh = new DBHelper(this);
+		SQLiteDatabase db = dbh.getReadableDatabase();
+
+		String selectQuery = "SELECT * FROM days;";
+		Cursor cursor = db.rawQuery(selectQuery, null);
+		cursor.moveToFirst();
+
+		if (!cursor.isAfterLast()) {
+			do {
+				String s = cursor.getString(2);
+				namelist.add(s);
+			} while (cursor.moveToNext());
+		}
+		db.close();
+
+		return namelist;
+	}
+
 	private ArrayList<Contact> getSelected() {
 		ArrayList<Contact> contactList = new ArrayList<Contact>();
-		
+
 		Contact contactItem;
 		DBHelper dbh = new DBHelper(this);
 		SQLiteDatabase db = dbh.getReadableDatabase();
@@ -65,10 +132,10 @@ public class ContactsPickerActivity extends Activity {
 			} while (cursor.moveToNext());
 		}
 		db.close();
-		
+
 		return contactList;
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 		//selection complete
@@ -85,12 +152,13 @@ public class ContactsPickerActivity extends Activity {
 		} catch (Exception e) {
 			Log.e("DB_INSERT","Failed to update selected contacts, "+e.toString());
 		}
-		
+
 		//do something with selected contacts...
-		Toast toast = Toast.makeText(getApplicationContext(), "Selected " + selectedContacts.size() + " contacts!", Toast.LENGTH_LONG);
+		Toast toast = Toast.makeText(getApplicationContext(), "Selected " + selectedContacts.size() + " contacts!", Toast.LENGTH_SHORT);
 		toast.show();
-		
+
 		listAdapter.deselectAll();
 		super.onBackPressed();
 	}
 }
+
--- Namnsdagar/src/se/sweddit/namnsdagar/contactspicker/Misc.java
+++ Namnsdagar/src/se/sweddit/namnsdagar/contactspicker/Misc.java
@@ -1,5 +1,9 @@
 package se.sweddit.namnsdagar.contactspicker;
 
+import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
 public class Misc {
 	
 	public static String[] ILLEGAL_CHARSEQ = {"@","&",".",";",":","!","/","_","|","~","Google","Android"};
@@ -12,5 +16,75 @@ public class Misc {
 		
 		return name.length() > 2;
 	}
+	
+	public static String getFirstName(String name) {
+		//förnamn och efternamn
+		String[] parts = name.split(" ");
+		
+		//om bara förnamn testa efter dubbelnamn
+		if (parts.length == 1) {
+			parts = name.split("-");
+		}
+		
+		
+		return parts[0].trim();
+	}
+	
+    private static int minimum(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+	
+	public static int getLevenshtein(String n1,String n2) {
+		//From http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Levenshtein_distance#Java
+		
+		String str1 = n1.toLowerCase();
+		String str2 = n2.toLowerCase();
+		
 
+        int[][] distance = new int[str1.length() + 1][str2.length() + 1];
+        
+        
+
+        for (int i = 0; i <= str1.length(); i++)
+                distance[i][0] = i;
+        
+        for (int j = 1; j <= str2.length(); j++)
+                distance[0][j] = j;
+
+        for (int i = 1; i <= str1.length(); i++)  {
+                for (int j = 1; j <= str2.length(); j++) {
+                        distance[i][j] = minimum(
+                                        distance[i - 1][j] + 1,
+                                        distance[i][j - 1] + 1,
+                                        distance[i - 1][j - 1]
+                                                        + ((str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0
+                                                                        : 1));
+                }
+        }
+
+        return distance[str1.length()][str2.length()];
+	}
+	
+	public static String[] getSimilarNames(String name, ArrayList<String> names, int numSuggestions) {
+		TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+
+		for (String n : names) {
+			int i = getLevenshtein(name, n);
+			map.put(i, n);
+		}
+		
+		String[] arr = new String[numSuggestions];
+		
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if ( map.firstEntry() != null) {
+				Entry<Integer,String> s = map.pollFirstEntry();
+				
+				arr[i] = s.getValue();
+			}
+		}
+
+		return arr;
+	}
 }

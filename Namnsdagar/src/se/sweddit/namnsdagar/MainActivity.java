88b23bc57a9c2892e5d3a0de075fcfdafd1c73b5
Johan Öbrell <johan@obrell.se>
2013-02-16 21:43:33 +0100

    Added main activity
    Added a main activity that launches with the app displaying todays
    names. Implemented new design as well as a btn to open settings.

--- Namnsdagar/src/se/sweddit/namnsdagar/MainActivity.java
+++ Namnsdagar/src/se/sweddit/namnsdagar/MainActivity.java
@@ -1,85 +1,70 @@
 package se.sweddit.namnsdagar;
 
-import se.sweddit.namnsdagar.contactspicker.ContactsPickerActivity;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.app.TimePickerDialog;
-import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
 	private static final String SETTINGS_NAME = "appSettings";
 	
 	private ProgressDialog progress;
-	private TimePickerDialog tDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        Date todayDate = new Date();
+        TextView dateView = (TextView)findViewById(R.id.textView2);
+        dateView.setText(DateFormat.getDateInstance(DateFormat.LONG).format(todayDate)+":");
+        
         final Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
-            	Intent myIntent = new Intent(v.getContext(), ContactsPickerActivity.class);
+            	Intent myIntent = new Intent(v.getContext(), Settings.class);
                 startActivityForResult(myIntent, 0);
             }
         });
         
-        TextView timeLink = (TextView) findViewById(R.id.textView2);
-        timeLink.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				SharedPreferences settings = getSharedPreferences(SETTINGS_NAME, 0);
-				int setHour = settings.getInt("remind_hour", 8);
-				int setMinute = settings.getInt("remind_minute", 0);
-				tDialog = new TimePickerDialog(MainActivity.this, new TimePickHandler(),
-						setHour,setMinute, true);
-				tDialog.show();
+        DBHelper dbh = new DBHelper(this);
+        SQLiteDatabase db = dbh.getReadableDatabase();
+        Calendar rightNow = Calendar.getInstance();
+        String namesToday = "";
+		try {
+			String selectQuery = "SELECT name FROM days WHERE month = " + (rightNow.get(Calendar.MONTH)+1) + " AND day = " + rightNow.get(Calendar.DAY_OF_MONTH) +";";
+			Cursor cursor = db.rawQuery(selectQuery, null);
+			cursor.moveToFirst();
+			if (!cursor.isAfterLast()) {
+				do {
+					if (namesToday.length()<2) {
+						namesToday = cursor.getString(0);
+					} else {
+						namesToday += ", " + cursor.getString(0);
+					}
+				} while (cursor.moveToNext());
 			}
-		});
-        
-        CheckBox modeSelect = (CheckBox) findViewById(R.id.checkBox1);
-        modeSelect.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-        	
-			@Override
-			public void onCheckedChanged(CompoundButton button, boolean isChecked) {
-				// TODO Auto-generated method stub
-	        	SharedPreferences settings = getSharedPreferences(SETTINGS_NAME, 0);
-	        	SharedPreferences.Editor editor = settings.edit();
-	        	editor.putBoolean("remind_mode", isChecked);
-	        	editor.commit();
-	        	Log.i("SET_MODE","Mode: "+isChecked);
-			}
-        });
-
-    	SharedPreferences settings = getSharedPreferences(SETTINGS_NAME, 0);
-    	int hourOfDay = settings.getInt("remind_hour", 8);
-    	int minute = settings.getInt("remind_minute", 0);
-    	boolean mode = settings.getBoolean("remind_mode", false);
-    	String displayMinute = minute+"";
-    	if (minute<10) displayMinute = "0"+minute;
-    	timeLink.setText(getResources().getString(R.string.set_time)+" ("+hourOfDay+":"+displayMinute+")");
-    	modeSelect.setChecked(mode);
+			db.close();
+		} catch (Exception e) {
+			Log.e("DB_GET","Unable to get selected contacts, "+e.toString());
+		}
+		TextView nameText = (TextView)findViewById(R.id.textView3);
+		nameText.setText(namesToday);
         
     	// 1. Instantiate an AlertDialog.Builder with its constructor
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -108,40 +93,7 @@ public class MainActivity extends Activity {
     	
         if (isFirstLaunch()) {
         	dialog.show();
-        } else {
-        	int selCount = getSelectedCount(this);
-        	
-        	button.setText(getResources().getString(R.string.choose_contacts)+" ("+selCount+")");
         }
-    }
-    
-    @Override
-    public void onRestart() {
-    	int selCount = getSelectedCount(this);
-        final Button button = (Button) findViewById(R.id.button1);
-    	button.setText(getResources().getString(R.string.choose_contacts)+" ("+selCount+")");
-    	super.onRestart();
-    }
-    
-    private int getSelectedCount(Context context) {
-    	int count = 0;
-    	
-    	DBHelper dbh = new DBHelper(context);
-    	SQLiteDatabase db = dbh.getReadableDatabase();
-    	
-		try {
-			String selectQuery = "SELECT COUNT(id_contact) AS count FROM selectedcontacts;";
-			Cursor cursor = db.rawQuery(selectQuery, null);
-			cursor.moveToFirst();
-			if (!cursor.isAfterLast()) {
-				count = cursor.getInt(0);
-			}
-			db.close();
-		} catch (Exception e) {
-			Log.e("DB_GET","Unable to get selected contacts, "+e.toString());
-		}
-    	
-    	return count;
     }
  
     private void loadData(boolean unofficial) {
@@ -159,31 +111,6 @@ public class MainActivity extends Activity {
 	    	editor.commit();
     	}
     	return isFirst;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    private class TimePickHandler implements OnTimeSetListener {
-
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        	SharedPreferences settings = getSharedPreferences(SETTINGS_NAME, 0);
-        	SharedPreferences.Editor editor = settings.edit();
-        	editor.putInt("remind_hour", hourOfDay);
-        	editor.putInt("remind_minute", minute);
-        	editor.commit();
-        	TextView timeText = (TextView) findViewById(R.id.textView2);
-        	String displayMinute = minute+"";
-        	if (minute<10) displayMinute = "0"+minute;
-        	timeText.setText(getResources().getString(R.string.set_time)+" ("+hourOfDay+":"+displayMinute+")");
-            tDialog.hide();
-            Log.i("SET_TIME","Hour: "+hourOfDay+", Minute: "+minute);
-        }
     }
     public class dataThread implements Runnable {
     	private boolean unofficial;

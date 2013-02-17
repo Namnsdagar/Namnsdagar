07c69747cae6f8c16d2cff34608dde8e9a7a5a72
Johan Öbrell <johan@obrell.se>
2013-02-17 21:39:12 +0100

    Rolled-back some stuff I overwrote.

--- Namnsdagar/src/se/sweddit/namnsdagar/NamedayService.java
+++ Namnsdagar/src/se/sweddit/namnsdagar/NamedayService.java
@@ -24,13 +24,13 @@ public class NamedayService extends IntentService {
 	public NamedayService() {
 		super("NamedayService");
 	}
-	
+
 	protected void worker(Intent intent) {
 		// Check namedays here...
 		Log.d("NamedayService", "NamedayService worker method running...");
 		checkContactsNameday();
 	}
-	
+
 	synchronized static PowerManager.WakeLock getWakeLock(Context context) {
 		if (wakeLock == null) {
 			// Requires android.permission.WAKE_LOCK permission
@@ -39,7 +39,7 @@ public class NamedayService extends IntentService {
 		}
 		return wakeLock;
 	}
-	
+
 	@Override
 	final protected void onHandleIntent(Intent intent) {
 		try {
@@ -50,36 +50,36 @@ public class NamedayService extends IntentService {
 			if (db.isOpen())
 				db.close();
 		}
-		
+
 		// Release wake lock when we're done.
 		try {
 			getWakeLock(this).release();
 		} catch (Exception e) {}
 	}
-	
-	
+
+
 	public void checkContactsNameday() {
-		
+
 		// Notify day before actual name day?
 		int dayOffset = 0;
 		SharedPreferences settings = this.getSharedPreferences(SETTINGS_NAME, 0);
 		boolean remind_mode = settings.getBoolean("remind_mode", false);
 		if (remind_mode)
 			dayOffset = 1;
-		
+
 		// Today it is...
 		Calendar calNow = new GregorianCalendar();
 		calNow.setTimeInMillis(System.currentTimeMillis());
-		
+
 		Calendar cal = new GregorianCalendar();
-		cal.set(Calendar.DAY_OF_YEAR, calNow.get(Calendar.DAY_OF_YEAR) + dayOffset);
-		cal.roll(Calendar.DAY_OF_YEAR, 1);
+		cal.set(Calendar.DAY_OF_YEAR, calNow.get(Calendar.DAY_OF_YEAR));
+		cal.roll(Calendar.DAY_OF_YEAR, dayOffset);
 		int month = cal.get(Calendar.MONTH) + 1;
 		int day = cal.get(Calendar.DAY_OF_MONTH);
-				
+
 		DBHelper dbh = new DBHelper(this);
     	db = dbh.getReadableDatabase();		
-		
+
 		// Välj de kontakter som matchar namnen för dagens namnsdag
 		String query = "SELECT * FROM selectedcontacts WHERE month='" + month + "' AND day='" + day + "'";
 		Cursor cursor = db.rawQuery(query, null);
@@ -93,12 +93,16 @@ public class NamedayService extends IntentService {
 			} while (!cursor.isLast());
 			names += cursor.getString(1);
 			new NamedayNotification((Context)this, names, remind_mode);
+			Log.d("NamedayService", "More than one contacts name day matched selected day ("+names+"). Notification sent.");
 		}
 		else if (cursor.getCount() == 1) {
 			cursor.moveToFirst();
 			names = cursor.getString(1);
 			new NamedayNotification((Context)this, names, remind_mode);
+			Log.d("NamedayService", "One contact's name day matched day ("+names+"). Notification sent.");
 		}
+		else
+			Log.d("NamedayService", "No contacts name day.");
 		db.close();		
 	}
 }
--- Namnsdagar/src/se/sweddit/namnsdagar/Settings.java
+++ Namnsdagar/src/se/sweddit/namnsdagar/Settings.java
@@ -14,7 +14,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;

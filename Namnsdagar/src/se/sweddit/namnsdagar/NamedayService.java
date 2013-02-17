a13ce8044c54863b973f5d68fb5fa619a88a99d1
Johan Öbrell <johan@obrell.se>
2013-02-17 22:35:06 +0100

    Added GPLv3-license text

--- Namnsdagar/src/se/sweddit/namnsdagar/NamedayService.java
+++ Namnsdagar/src/se/sweddit/namnsdagar/NamedayService.java
@@ -1,10 +1,23 @@
 package se.sweddit.namnsdagar;
+/*
+This file is part of Svenska Namnsdagar
 
-import java.util.ArrayList;
+Svenska Namnsdagar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Svenska Namnsdagar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Svenska Namnsdagar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.List;
-
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
